93a11b99fbee8e1e286cf2e5892cef81a873a93a
Ya-wen Alice <yawenalice@gmail.com>
2016-11-18 17:04:21 -0500

    update

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/public'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 
 app.use('/', require('./routes'));
 
